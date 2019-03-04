81d5e8edfe6905105a13f135c58a3f65b866fd07
Cynthia <1725877349@qq.com>
2019-03-04 23:18:30 +0800

    Init

---
+++ src/Cynthia.Blogger.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cynthia.Blogger.Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
