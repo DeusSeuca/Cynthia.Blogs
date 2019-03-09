551e456a7dcfe17fb68ac24ab46d732ce993c489
Cynthia <1725877349@qq.com>
2019-03-09 13:08:00 +0800

    ChangePort

--- src/Cynthia.Blogs.Server/Program.cs
+++ src/Cynthia.Blogs.Server/Program.cs
@@ -19,6 +19,7 @@ namespace Cynthia.Blogs.Server
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:1999")
                 .UseStartup<Startup>();
     }
 }
